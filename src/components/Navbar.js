0843acfb3dc27c86e2f70e876eb8d03ec1d0a116
KingShahrukh <srks50940@gmail.com>
2024-05-02 20:15:56 +0530

    MY-Todo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Navbar = () => {
   return (
     <>
-      <nav className=" flex justify-between bg-slate-700 text-white py-2 h-10">
+      <nav className=" flex justify-between bg-slate-700 text-white py-2 ">
         <div className="logo">
           <span className=" font-bold text-xl mx-8">Tasks-Manager</span>
         </div>
